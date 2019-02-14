d1340a0e5cf39f485971c09bb8299d03b3f76b36
srdrabx <devalladolidoficial@gmail.com>
2019-02-14 12:29:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
        $date = new DateTime('now', new DateTimeZone('UTC'));
 ?>
 {
- current_date: "<?php echo $date->format('Y_1_d'); ?>",
+ "current_date": "<?php echo $date->format('Y_1_d'); ?>"
 }
